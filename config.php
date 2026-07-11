274466a1f3fbf6f01aca267537ea24f938c4aa80
zannhengsut642-web <zannhengsut642@gmail.com>
2026-07-11 08:37:34 +0700

    Add files via upload

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '');
-define('MONETAG_ZONE', '');
-define('MINI_APP_URL', '');
+define('8877567340:AAGouj-UCWZQIC7ySeCdps9i1UE6BTNsR1Q', '');
+define('11268223', '');
+define('https://like-ff-api-one.vercel.app/', '');
 define('DB_PATH', __DIR__ . '/database.db');
