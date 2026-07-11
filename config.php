596dcfba1bc795de54c562a6b62024516b14de71
zannhengsut642-web <zannhengsut642@gmail.com>
2026-07-11 12:25:38 +0700

    Delete config.php

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('8877567340:AAGouj-UCWZQIC7ySeCdps9i1UE6BTNsR1Q', '');
-define('11268223', '');
-define('https://like-ff-api-one.vercel.app/', '');
-define('DB_PATH', __DIR__ . '/database.db');
